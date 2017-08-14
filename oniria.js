5c14423bc8915cf3f35008fe7bd30d8e157cecc5
Biquet <Baka-killer@hotmail.fr>
2017-08-14 19:38:38 +0200

    Prepared start animation and base menu

--- oniria.js
+++ oniria.js
@@ -8,7 +8,7 @@ let win;
 
 function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 1280, height: 720, frame:false});
+    win = new BrowserWindow({width: 1280, height: 720, frame:false, backgroundColor:'#000000'});
 
     // and load the index.html of the app.
     win.loadURL(url.format({
@@ -18,7 +18,7 @@ function createWindow () {
     }));
 
     // Open the DevTools.
-    // win.webContents.openDevTools();
+    win.webContents.openDevTools();
 
     // Emitted when the window is closed.
     win.on('closed', () => {
